d453e3cc5ee6c7eca609e9f85972ffedef1742aa
Ramprit Sahani <rampritsahani@outlook.in>
2018-12-27 20:59:51 +0530

    react ejected application

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "jquery/dist/jquery";
-import "popper.js/dist/popper";
-import "bootstrap/dist/js/bootstrap.min";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
